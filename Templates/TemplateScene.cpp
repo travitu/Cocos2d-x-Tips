9df28b64ab623579130fad0fdf3ec891d9143710
travitu <travitu84@gmail.com>
2016-03-04 23:39:30 +0900

    Add templates.

---
+++ Templates/TemplateScene.cpp
@@ -0,0 +1,92 @@
+//
+//  TemplateScene.cpp
+//
+//
+//
+
+#include "TemplateScene.h"
+
+USING_NS_CC;
+
+Scene* TemplateScene::createScene()
+{
+    // Create scene.
+    auto scene = Scene::create();
+    
+    // Create layer.
+    auto layer = TemplateScene::create();
+    
+    // Add layer on the scene.
+    scene->addChild(layer);
+    
+    // Return the pointer of scene.
+    return scene;
+}
+
+bool TemplateScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() ) {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+    
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+    
+    // add a "close" icon to exit the progress. it's an autorelease object
+    auto closeItem = MenuItemImage::create(
+                                           "CloseNormal.png",
+                                           "CloseSelected.png",
+                                           CC_CALLBACK_1(TemplateScene::menuCloseCallback, this));
+    
+    closeItem->setPosition(Point(origin.x + visibleSize.width - closeItem->getContentSize().width/2 ,
+                                 origin.y + closeItem->getContentSize().height/2));
+    
+    // create menu, it's an autorelease object
+    auto menu = Menu::create(closeItem, NULL);
+    menu->setPosition(Point::ZERO);
+    this->addChild(menu, 1);
+    
+    /////////////////////////////
+    // 3. add your codes below...
+    
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    
+    // position the label on the center of the screen
+    label->setPosition(Vec2(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - label->getContentSize().height));
+    
+    // add the label as a child to this layer
+    this->addChild(label, 1);
+    
+    // add "HelloWorld" splash screen"
+    auto sprite = Sprite::create("HelloWorld.png");
+    
+    // position the sprite on the center of the screen
+    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    
+    // add the sprite as a child to this layer
+    this->addChild(sprite, 0);
+    
+    return true;
+    
+}
+
+void TemplateScene::menuCloseCallback(Ref* pSender)
+{
+    Director::getInstance()->end();
+    
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#elif (CC_TARGET_PLATFORM == CC_PLATFORM_ANDROID)
+    
+#endif
+}
